4f43b2b137874ac88e23b167d8b23105251e9ef8
Gustavo PAredez <picohbk322@gmail.com>
2022-06-24 06:04:24 -0300

    23

---
+++ src/main/java/spring/Controller/Usuario.java
@@ -0,0 +1,12 @@
+package spring.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Usuario {
+    @RequestMapping("/")
+    public String prueba(){
+        return "Hola mundo";
+    }
+}
